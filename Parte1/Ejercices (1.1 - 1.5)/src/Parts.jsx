ca8cec1a466ae2222de7219ad46dbc5ac21773e5
alvarezpk <alvarezpachecosantiagopk@gmail.com>
2024-03-08 19:23:51 -0300

    Part0 and Ejercices 1.1-1.2 of Part1
    This is my first Commit, here are the exercises from part0 and 1.1-1.2 from part 1

---
+++ Parte1/Ejercices (1.1 - 1.5)/src/Parts.jsx
@@ -0,0 +1,8 @@
+
+const Parts = (props) => {
+    return(
+        <p>{props.parts} {props.exercises}</p>
+    )
+}
+
+export default Parts
---
+++ Parte1/Ejercices (1.1 - 1.5)/src/Total.jsx
@@ -0,0 +1,8 @@
+
+const Total = (props) => {
+    return (
+        <p>Number of exercices: {props.total}</p>
+    )
+}
+
+export default Total
